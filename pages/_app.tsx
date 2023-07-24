b5dd3a50d6a043b9898a40f5fda7c7593b115ea1
acomarcho <acoxstpd@gmail.com>
2023-07-24 18:51:41 +0700

    Feat: Setup fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Outfit, Plus_Jakarta_Sans } from "next/font/google";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  variable: "--font-outfit",
+});
+const plusJakartaSans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  variable: "--font-plus-jakarta-sans",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${outfit.variable} ${plusJakartaSans.variable}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
