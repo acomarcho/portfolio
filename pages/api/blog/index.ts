7fb7f4799499f8a89cb6515e3e7f925380b8c94d
acomarcho <acoxstpd@gmail.com>
2023-07-25 14:49:48 +0700

    Feat: Add API to read blog posts

---
+++ pages/api/blog/index.ts
@@ -0,0 +1,64 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import asyncFs from "fs/promises";
+import path from "path";
+import grayMatter from "gray-matter";
+
+type Blog = {
+  slug: string;
+  title: string;
+  summary: string;
+  date: string;
+};
+
+type GetBlogListResponse = {
+  blogs: Blog[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<GetBlogListResponse>
+) {
+  if (req.method === "GET") {
+    // Crawl markdown files
+    const markdownFiles: string[] = [];
+
+    const crawlFolderForMarkdownFiles = (folderPath: string) => {
+      const files = fs.readdirSync(folderPath);
+
+      for (const file of files) {
+        const filePath = path.join(folderPath, file);
+
+        if (fs.lstatSync(filePath).isDirectory()) {
+          crawlFolderForMarkdownFiles(filePath);
+        } else {
+          if (path.extname(file).toLowerCase() === ".md") {
+            markdownFiles.push(filePath);
+          }
+        }
+      }
+    };
+
+    crawlFolderForMarkdownFiles(path.resolve("./public", "blog"));
+
+    // Read the actual markdown file
+    let blogs: Blog[] = await Promise.all(
+      markdownFiles.map(async (path) => {
+        const fileContent = await asyncFs.readFile(path, "utf-8");
+
+        const parsedContent = grayMatter(fileContent);
+        return {
+          slug: parsedContent.data.slug,
+          title: parsedContent.data.title,
+          summary: parsedContent.data.summary,
+          date: parsedContent.data.date,
+        };
+      })
+    );
+
+    res.status(200).json({ blogs });
+  } else {
+    res.status(405).end();
+  }
+}
---
+++ public/blog/hello-world/hello-world.md
@@ -0,0 +1,10 @@
+---
+slug: hello-world
+title: Et repellat magnam fugit sunt et tempore officiis laudantium.
+summary: Magnam architecto harum nemo eius. Nesciunt est aut dolores omnis similique nostrum. Ea earum similique iusto quas. Ut ut dicta omnis est qui dicta modi eos totam. Quos ipsa eos vitae perferendis ratione incidunt est.
+date: 25 Juli 2023
+---
+
+# Hello world!
+
+Beatae maiores tempore aspernatur fugit expedita aut. Nihil asperiores assumenda impedit vel. Suscipit dolorem voluptas inventore unde. Ea et harum. Iure eveniet perspiciatis et ab vero repellendus.
